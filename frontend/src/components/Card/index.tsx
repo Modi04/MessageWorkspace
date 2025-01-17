1c2a9df8352d7b6c0f6ad533f4e941b2c04d851c
poqopo <block.poqopo@gmail.com>
2025-01-18 00:13:20 +0900

    Add Friends page and Card

---
+++ frontend/src/components/Card/index.tsx
@@ -0,0 +1,7 @@
+import { useRouter } from 'next/router';
+
+export default function Card() {
+  const router = useRouter();
+
+  return <div className="bg-green"></div>;
+}
--- frontend/src/pages/friends/index.tsx
+++ frontend/src/pages/friends/index.tsx
@@ -1,10 +1,12 @@
 import { useRouter } from 'next/router';
+import Card from '../../components/Card';
 
 export default function Index() {
   const router = useRouter();
 
   return (
     <div className="text-white text-center">
+      <Card></Card>
       <div>Hello!!!!</div>
     </div>
   );
