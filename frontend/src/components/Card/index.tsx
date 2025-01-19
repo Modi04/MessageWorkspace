e75d6b6443a0c12978937e83a7125971164f5ab5
poqopo <block.poqopo@gmail.com>
2025-01-19 15:38:05 +0900

    SetProfile Page and Make UserList and change Colors

--- frontend/src/components/Card/index.tsx
+++ frontend/src/components/Card/index.tsx
@@ -1,19 +1,21 @@
 import { useRouter } from 'next/router';
-import { BsThreeDotsVertical } from "react-icons/bs";
+import { BsThreeDotsVertical } from 'react-icons/bs';
 
 export default function Card() {
   const router = useRouter();
 
-  return <div className="border-b border-slate-700 grid grid-cols-[1.5fr_8fr_1fr] grid-rows-1 gap-2 bg-black h-20 place-items-center">
-    <div className="text-white rounded-full bg-[#4F378B] flex items-center justify-center font-mono w-12 h-12">
-      A
+  return (
+    <div className="border-b border-slate-700 grid grid-cols-[1.5fr_8fr_1fr] grid-rows-1 gap-2 bg-black h-20 place-items-center">
+      <div className="text-black rounded-full bg-[#A4FF11] bg-opacity-75 flex items-center justify-center font-mono w-12 h-12">
+        A
+      </div>
+      <div className="text-white text-left flex justify-start w-full flex-col">
+        <div>Unknown</div>
+        <div>profile line</div>
+      </div>
+      <div className="">
+        <BsThreeDotsVertical />
+      </div>
     </div>
-    <div className="text-white text-left flex justify-start w-full flex-col">
-      <div>Unknown</div>
-      <div>profile line</div>
-    </div>
-    <div className=''>
-      <BsThreeDotsVertical />
-    </div>
-  </div>;
+  );
 }
