31a50c0b53cf1987203be9c60de8a9a5abdb61c9
poqopo <block.poqopo@gmail.com>
2025-01-20 17:06:25 +0900

    Edit Footer Routes logic

---
+++ frontend/src/components/AddFriend.tsx
@@ -0,0 +1,11 @@
+import { useRouter } from 'next/router';
+
+export default function AddFriend() {
+  const router = useRouter();
+
+  return (
+    <div className="fixed h-screen w-1/4 max-w-[378px] bg-white text-white">
+      <div className="bg-white text-black">Menu!!!!</div>
+    </div>
+  );
+}
--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -1,58 +1,50 @@
 import { useRouter } from 'next/router';
-import { MdOutlinePersonOutline, MdSettings } from "react-icons/md";
-import { FiMessageSquare } from "react-icons/fi";
+import { MdOutlinePersonOutline, MdSettings } from 'react-icons/md';
+import { FiMessageSquare } from 'react-icons/fi';
 import { useEffect, useState } from 'react';
 
 export default function Footer() {
   const router = useRouter();
+  const { identity } = router.query;
 
-  const [isSelected, setIsSelected] = useState({
-    friends: false,
-    messages: false,
-    settings: false,
-  });
-
-  useEffect(() => {
-    if (router.pathname === '/friends') {
-      setIsSelected({friends: true, messages: false, settings: false});
-    } else if (router.pathname === '/messages') {
-      setIsSelected({friends: false, messages: true, settings: false});
-    } else if (router.pathname === '/settings') {
-      setIsSelected({friends: false, messages: false, settings: true});
-    }
-    }, [router.pathname]);
-
-  const handleNavigation = (path: string, menu: keyof typeof isSelected) => {
+  const handleNavigation = (path: string) => {
     if (router.pathname === path) return;
-    router.push(path);
-    setIsSelected({
-      friends: menu === 'friends',
-      messages: menu === 'messages',
-      settings: menu === 'settings',
-    });
+    router.push({ pathname: path, query: { identity: identity } });
   };
 
   return (
     <div className="w-full max-w-[600px] fixed bottom-0 h-20 bg-[#211F26] flex justify-around items-center">
-      <div className="flex flex-col items-center cursor-pointer"
-      onClick={() => handleNavigation('/friends', 'friends')}>
-      <div className={`flex justify-center items-center ${isSelected.friends ? 'bg-gray-300 bg-opacity-30 rounded-3xl' : ''} w-16 h-10 p-2`}>
-        <MdOutlinePersonOutline size='24' />
-      </div>
+      <div
+        className="flex flex-col items-center cursor-pointer"
+        onClick={() => handleNavigation('friends')}
+      >
+        <div
+          className={`flex justify-center items-center ${router.pathname == '/friends' ? 'bg-gray-300 bg-opacity-30 rounded-3xl' : ''} w-16 h-10 p-2`}
+        >
+          <MdOutlinePersonOutline size="24" />
+        </div>
         <p>Friends</p>
       </div>
-     <div className="flex flex-col items-center cursor-pointer"
-     onClick={() => handleNavigation('/messages', 'messages')}>
-     <div className={`flex justify-center items-center ${isSelected.messages ? 'bg-gray-300 bg-opacity-30 rounded-3xl' : ''} w-16 h-10 p-2`}>
-        <FiMessageSquare  size='24' />
-      </div>
+      <div
+        className="flex flex-col items-center cursor-pointer"
+        onClick={() => handleNavigation('chats')}
+      >
+        <div
+          className={`flex justify-center items-center ${router.pathname == '/chats' || router.pathname == '/messages' ? 'bg-gray-300 bg-opacity-30 rounded-3xl' : ''} w-16 h-10 p-2`}
+        >
+          <FiMessageSquare size="24" />
+        </div>
         <p>Message</p>
       </div>
-      <div className="flex flex-col items-center cursor-pointer"
-      onClick={() => handleNavigation('/settings', 'settings')}>
-      <div className={`flex justify-center items-center ${isSelected.settings ? 'bg-gray-300 bg-opacity-30 rounded-3xl' : ''} w-16 h-10 p-2`}>
-        <MdSettings  size='24' />
-      </div>
+      <div
+        className="flex flex-col items-center cursor-pointer"
+        onClick={() => handleNavigation('settings')}
+      >
+        <div
+          className={`flex justify-center items-center ${router.pathname == '/settings' ? 'bg-gray-300 bg-opacity-30 rounded-3xl' : ''} w-16 h-10 p-2`}
+        >
+          <MdSettings size="24" />
+        </div>
         <p>Setting</p>
       </div>
     </div>
