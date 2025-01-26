3f531f95970b8987c8392073458e1dc293d88673
poqopo <block.poqopo@gmail.com>
2025-01-26 09:03:23 +0900

    For merge

--- frontend/src/components/UserList.tsx
+++ frontend/src/components/UserList.tsx
@@ -1,5 +1,6 @@
 import { useRouter } from 'next/router';
 import Card from './Card';
+import { HiMiniBars3 } from "react-icons/hi2";
 
 export default function UserList({ title, setIsSelected, setValue, contents }) {
   const router = useRouter();
@@ -11,11 +12,16 @@ export default function UserList({ title, setIsSelected, setValue, contents }) {
 
   return (
     <div className="w-full h-full">
-      <div className="text-[23px] p-3 text-[#E6E0E9]">{title}</div>
+      <div className='flex items-center'>
+        <div className="text-[23px] p-3 text-[#E6E0E9]">{title}</div>
+        <button className='ml-auto mr-4'>
+          <HiMiniBars3 size={24}/>
+        </button>
+      </div>
       {contents.map((message, index) => (
         <div className="w-full border-b border-slate-600">
           <button
-            className="w-full "
+            className="w-full"
             key={index}
             onClick={() => onCardClick(message.id)} // 화살표 함수로 래핑
           >
